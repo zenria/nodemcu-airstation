8230af09d57de90214b54683294797856aa568c0
Philippe GASSMANN <philoops@gmail.com>
2017-02-14 23:43:34 +0100

    Reconnect if lost connection

--- mqttReporter.lua
+++ mqttReporter.lua
@@ -17,44 +17,60 @@ local function sendHeap()
 	mqttReporter.sendValue("/heapFree", node.heap())
 end
 
-
+local reconnect = nil
 
 function mqttReporter.connect(host, callback)
 	local statusTopic = getTopic("/status")
     local rpcTopic = getTopic("/exec")
-	client = mqtt.Client(clientId, 120)
-	-- will go offline if disconnected
-	client:lwt(statusTopic, STATUS_OFFLINE, 0, true)
+    reconnect = function(c2)
 
-    client:on("message", function(client, topic, data) 
-        if topic == rpcTopic then
-            node.input(data)
-        end
-    end)
+        if log == nil then log = print end
     
-    print("MQTT - Connecting to "..host)
-	client:connect(host, 1883, function(c)
-		print("MQTT - Connected")
-		-- set online
-		c:publish(statusTopic, STATUS_ONLINE, 0, 0)
-        -- subscribe to rcp topic
-        c:subscribe(rpcTopic, 2)
-		-- setup heap reporter
-		local timer = tmr.create()
-		timer:register(120000, tmr.ALARM_AUTO, sendHeap)
-	    timer:start()
-	    -- report heap now
-	    sendHeap()
-		if callback then
-			callback()
-		end
-	end, function(c, error)
-        print("Unable to connect to mqtt broker: "..error)
-	end)
+		client = mqtt.Client(clientId, 120)
+		-- will go offline if disconnected
+		client:lwt(statusTopic, STATUS_OFFLINE, 0, true)
+
+	    client:on("message", function(client, topic, data) 
+	        if topic == rpcTopic then
+	            node.input(data)
+	        end
+	    end)
+	    
+	    log("MQTT - Connecting to "..host)
+		client:connect(host, 1883, function(c)
+			log("MQTT - Connected")
+			-- set online
+			c:publish(statusTopic, STATUS_ONLINE, 0, 0)
+	        -- subscribe to rcp topic
+	        c:subscribe(rpcTopic, 2)
+			-- setup heap reporter
+			local timer = tmr.create()
+			timer:register(120000, tmr.ALARM_AUTO, sendHeap)
+		    timer:start()
+		    -- report heap now
+		    sendHeap()
+			if c2 then
+				c2()
+			end
+		end, function(c, error)
+	        log("Unable to connect to mqtt broker: "..error)
+		end)
+	end
+	reconnect(callback)
 end
 
 function mqttReporter.sendValue(path, value)
-	client:publish(getTopic(path), value, 0, 0)
+	local function pub()
+		return client:publish(getTopic(path), value, 0, 0)
+	end
+	if not pub() then
+        log("Unable to send to MQTT "..path.." "..value..", try to reconnect")
+		reconnect(function()
+			if not pub() then
+				log("Unable to send to MQTT "..path.." "..value)
+            end
+		end)
+	end
 end
 
 --return mqttReporter
